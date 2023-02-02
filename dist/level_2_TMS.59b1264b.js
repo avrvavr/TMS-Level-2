086e4e12b5f7ff1d0ec888656a570e4bb2b3aa29
avrvavr <avrvavr@gmail.com>
2023-02-02 20:23:03 +0300

    first commit

---
+++ dist/level_2_TMS.59b1264b.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open");
+    });
+});
+
+//# sourceMappingURL=level_2_TMS.59b1264b.js.map
